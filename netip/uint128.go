40826a6ade95e74be0785e91196fd46523c8e6ab
Ryuju0911 <pinebook0911@gmail.com>
2023-12-02 17:59:02 +0900

    Add netip.Addr. Compared to the [net.IP] type, [Addr] type takes less memory, is immutable, and is comparable.

---
+++ netip/uint128.go
@@ -0,0 +1,14 @@
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package netip
+
+// uint128 represents a uint128 using two uint64s.
+//
+// When the methods below mention a bit number, bit 0 is the most
+// significant bit (in hi) and bit 127 is the lowest (lo&1).
+type uint128 struct {
+	hi uint64
+	lo uint64
+}
